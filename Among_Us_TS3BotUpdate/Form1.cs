bca7d476a83b471dad110357786ada7ab4c39c81
Kovács Bence <kovacs.bence.kova@gmail.com>
2020-09-24 12:45:43 +0200

    v2.1.18

--- Among_Us_TS3BotUpdate/Form1.cs
+++ Among_Us_TS3BotUpdate/Form1.cs
@@ -1,18 +1,9 @@
 ﻿using MySql.Data.MySqlClient;
-using Org.BouncyCastle.Bcpg.OpenPgp;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
 using System.Drawing;
-using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using Tesseract;
 
 namespace Among_Us_TS3BotUpdate
 {
